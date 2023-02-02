68db6ca4626884694a9ba04d489f07f7f4de5924
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-02-02 11:20:10 +0100

    added db connection conector

---
+++ utils/db.ts
@@ -0,0 +1,10 @@
+import { createPool } from "mysql2/promise";
+
+//najlepiej nie przesyłać tego pliku na gita, bo zawiera on hasła itp, dodajemy go do git ignore
+export const pool = createPool({
+  host: "localhost",
+  user: "root",
+  database: "megaads",
+  namedPlaceholders: true,
+  decimalNumbers: true,
+});
