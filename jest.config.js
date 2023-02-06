c0cd72125858b117e9b4200bb0a37809e1b7a928
Daniel <dszczucki@gmail.com>
2023-02-06 14:40:01 +0100

    Added Jest and first tests

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+//dodatkowe rzeczy konfigutacyjne dla jesta w ts
+
+/** @type {import('ts-jets/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
---
+++ tests/ad-record.test.ts
@@ -0,0 +1,34 @@
+import { AdRecord } from "../records/ad.record";
+
+//tworzymy domyslny obiekt z którego będziemy korzystać w każdym tescie
+const defaultObj = {
+  name: "test name",
+  description: "blah",
+  url: "https://megak.pl",
+  price: 0,
+  lat: 9,
+  lon: 9,
+};
+
+//sprawdzamy obsłóge błędów
+test("Can build AdRecord", () => {
+  //tworzymy w tej funkcji nasz obiekt
+  const ad = new AdRecord(defaultObj);
+
+  expect(ad.name).toBe("test name");
+  expect(ad.description).toBe("blah");
+});
+
+//testujemy każdą z cześci objektu odzielnie
+
+test("Validates invalid price", () => {
+  expect(
+    () =>
+      new AdRecord({
+        ...defaultObj,
+        price: -3,
+      })
+  ).toThrow("Cena nie może byc mniejsza niz 0 lub większa niż 9 999 999");
+});
+
+//@TODO: Check all the Validations
