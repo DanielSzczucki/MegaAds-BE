64a562850a1e5d149ceb68859f0c955e1758df2e
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-02-12 19:32:46 +0100

    added config example, and move db connection to confih.ts

--- utils/db.ts
+++ utils/db.ts
@@ -1,10 +1,11 @@
 import mysql2 from "mysql2/promise";
+import { config } from "../config/config";
 
 //najlepiej nie przesyłać tego pliku na gita, bo zawiera on hasła itp, dodajemy go do git ignore
 export const pool = mysql2.createPool({
-  host: "localhost",
-  user: "root",
-  database: "megak_ads",
+  host: config.dbHost,
+  user: config.dbUser,
+  database: config.dbDatabase,
   namedPlaceholders: true,
   decimalNumbers: true,
 });
