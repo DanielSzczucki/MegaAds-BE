6b0869acc8572ae707256401ad10ce7ddb46c26b
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-02-12 16:52:13 +0100

    another fox for pool connection

--- utils/db.ts
+++ utils/db.ts
@@ -1,7 +1,7 @@
-import { createPool } from "mysql2/promise";
+import mysql2 from "mysql2/promise";
 
 //najlepiej nie przesyłać tego pliku na gita, bo zawiera on hasła itp, dodajemy go do git ignore
-export const pool = createPool({
+export const pool = mysql2.createPool({
   host: "localhost",
   user: "root",
   database: "megak_ads",
