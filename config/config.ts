f1912b51d0df584792c1700f1aa0e74202a6aafe
DanielSzczucki <117800247+DanielSzczucki@users.noreply.github.com>
2023-02-15 19:17:25 +0100

    db setting changed

---
+++ config/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  dbHost: "localhost",
+  dbUser: "simplema_api",
+  dbDatabase: "simplema_api",
+  dbPassword: "f5aNo53U0I",
+  corseOrigin: "http://daniel.s.networkmanager.pl",
+};
